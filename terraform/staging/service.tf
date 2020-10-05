f0a1fe953b8981fbda9c25885b83ef5ce1bf6aa3
Marcin Struś <marcin.strus@makimo.pl>
2020-10-05 15:19:04 +0200

    Format terraform files

--- terraform/staging/service.tf
+++ terraform/staging/service.tf
@@ -1,22 +1,22 @@
 data "aws_ecs_cluster" "kopytko" {
-    cluster_name = var.ecs_cluster
+  cluster_name = var.ecs_cluster
 }
 
 resource "aws_ecs_service" "name_microservice" {
-    name = "${var.project_name}-name-microservice"
-    cluster = data.aws_ecs_cluster.kopytko.id
-    desired_count = 1
-    task_definition = aws_ecs_task_definition.name_microservice.arn
+  name            = "${var.project_name}-name-microservice"
+  cluster         = data.aws_ecs_cluster.kopytko.id
+  desired_count   = 1
+  task_definition = aws_ecs_task_definition.name_microservice.arn
 
-    network_configuration {
-        subnets = var.subnet_ids
-        security_groups = var.security_group_ids
-        assign_public_ip = true
-    }
+  network_configuration {
+    subnets          = var.subnet_ids
+    security_groups  = var.security_group_ids
+    assign_public_ip = true
+  }
 
-    launch_type = "FARGATE"
+  launch_type = "FARGATE"
 
-    deployment_controller {
-        type = "ECS"
-    }
+  deployment_controller {
+    type = "ECS"
+  }
 }
--- terraform/staging/variables.tf
+++ terraform/staging/variables.tf
@@ -39,7 +39,7 @@ variable "task_execution_role_name" {
 
 variable "common_tags" {
   default = {
-    Environment: "Staging",
-    Project: "PlanningBot",
+    Environment : "Staging",
+    Project : "PlanningBot",
   }
 }
